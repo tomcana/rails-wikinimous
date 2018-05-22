77b2ae6a30dfdc593e3524519fcdbbf4615d8c72
Tomomi Shiba <follwthedot@gmail.com>
2018-05-22 17:22:07 +0900

    working on Rails Wikinimous

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  def index         # GET /restaurants
+  end
+
+  def show          # GET /restaurants/:id
+  end
+
+  def new           # GET /restaurants/new
+  end
+
+  def create        # POST /restaurants
+  end
+
+  def edit          # GET /restaurants/:id/edit
+  end
+
+  def update        # PATCH /restaurants/:id
+  end
+
+  def destroy       # DELETE /restaurants/:id
+  end
 end
